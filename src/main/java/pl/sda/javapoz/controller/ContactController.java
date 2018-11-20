2817084628cc9c26930d2eaa898b756265fcd407
Jetoxixoxorix <technoamfa13@gmail.com>
2018-11-20 17:56:39 +0100

    testing commit

--- src/main/java/pl/sda/javapoz/controller/ContactController.java
+++ src/main/java/pl/sda/javapoz/controller/ContactController.java
@@ -12,8 +12,12 @@ import pl.sda.javapoz.service.NavbarLinkService;
 @Controller
 public class ContactController {
 
-    @Autowired
     private NavbarLinkService navbarLinkService;
+
+    @Autowired
+    public ContactController(NavbarLinkService navbarLinkService) {
+        this.navbarLinkService = navbarLinkService;
+    }
 
     @GetMapping("/contact")
     public ModelAndView showContact(ModelAndView modelAndView){
