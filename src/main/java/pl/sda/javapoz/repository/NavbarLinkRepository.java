c81c29e48383d4e14c7f8b09c3cc3e2514e8441d
Jetoxixoxorix <technoamfa13@gmail.com>
2018-11-21 22:47:21 +0100

    changes in repositories

--- src/main/java/pl/sda/javapoz/repository/NavbarLinkRepository.java
+++ src/main/java/pl/sda/javapoz/repository/NavbarLinkRepository.java
@@ -1,10 +1,10 @@
 package pl.sda.javapoz.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import pl.sda.javapoz.model.NavbarLink;
 
-public interface NavbarLinkRepository extends CrudRepository<NavbarLink,Long>{
-
+@Repository
+public interface NavbarLinkRepository extends CrudRepository<NavbarLink, Long> {
     public NavbarLink findByName(String name);
-
 }
