3e1dc52eb6b29d0a119ed352cbad1c197d8beeb5
Mati <kaczmarek.mateusz@o2.pl>
2019-01-07 16:38:37 +0100

    add button and form to product template

--- src/main/java/pl/sda/javapoz/controller/ProductController.java
+++ src/main/java/pl/sda/javapoz/controller/ProductController.java
@@ -39,19 +39,16 @@ public class ProductController {
     }
 
     @PostMapping("/product/{id}")
-    public ModelAndView orderProduct(@PathVariable Long id, @ModelAttribute ProductOrderEntity productOrder, ModelAndView modelAndView) {
+    public ModelAndView addProductToCart(@PathVariable Long id,
+                                     @RequestParam(value = "productCount") String productCount,
+                                     @RequestParam(value = "productName") String productName,
+                                     ModelAndView modelAndView) {
         modelAndView.setViewName("product");
-        ProductEntity productById = productService.findProductById(id);
-        Date productOrderStart = productOrder.getOrderStart();
-        Date productOrderEnd = productOrder.getOrderEnd();
-        UserEntity loggedUser = sessionService.getCurrentUser();
 
-        boolean availableToOrder = productOrderService.isProductAvailableToOrder(id, productOrderStart, productOrderEnd);
-        if (availableToOrder) {
-            modelAndView.addObject("info", new Info("produkt zamówiony poprawnie", true));
-            productOrderService.saveOrder(loggedUser, productById, productOrderStart, productOrderEnd);
+        if (productCount.isEmpty() || Integer.parseInt(productCount) < 1) {
+            modelAndView.addObject("info", new Info("Nieprawidłowa ilość ", false));
         } else {
-            modelAndView.addObject("info", new Info("produkt niedostępny w tym okresie", false));
+            modelAndView.addObject("info", new Info("Dodano do koszyka " + productCount + " " + productName , true));
         }
 
         return productPage(id, modelAndView);
