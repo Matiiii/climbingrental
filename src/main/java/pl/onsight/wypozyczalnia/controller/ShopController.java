f84d10a1cc5e39a0f959ecfdf80eff4d6208af6b
Mati <kaczmarek.mateusz@o2.pl>
2019-01-11 13:15:14 +0100

    remove duplicate code in shopController

--- src/main/java/pl/onsight/wypozyczalnia/controller/ShopController.java
+++ src/main/java/pl/onsight/wypozyczalnia/controller/ShopController.java
@@ -37,32 +37,30 @@ public class ShopController {
         modelAndView.addObject("product", new ProductEntity());
         modelAndView.addObject("filterProducts", new FilterProducts());
 
-        boolean hasNoParameters = "".equals(prodName) && "".equals(dateFilter) && cart.getDate() == null;
-        boolean hasOnlyProductName = !"".equals(prodName) && "".equals(dateFilter) && cart.getDate() == null;
-        boolean hasOnlyDates = "".equals(prodName) && (!"".equals(dateFilter) || cart.getDate() != null);
+        boolean hasNoParameters = prodName.equals("") && dateFilter.equals("") && cart.getDate() == null;
+        boolean hasOnlyProductName = !prodName.equals("") && dateFilter.equals("") && cart.getDate() == null;
+        boolean hasOnlyDates = prodName.equals("") && (!dateFilter.equals("") || cart.getDate() != null);
 
         if (hasNoParameters) {
             modelAndView.addObject("countProducts", productService.countAllProductsByName());
+
         } else if (hasOnlyProductName) {
             modelAndView.addObject("countProducts", productService.countAllProductsByNameFiltered(prodName));
             modelAndView.addObject("info", new Info("Produkty zawierające frazę: <b>" + prodName + "</b>", true));
+
         } else if (hasOnlyDates) {
             if (dateFilter.isEmpty()) {
-                modelAndView.addObject("countProducts", productService.countAllAvailableProductsByName(cart.getDate()));
-                modelAndView.addObject("info", new Info("Produkty dostępne: <b>" + cart.getDate() + "</b>", true));
-            } else {
-                modelAndView.addObject("countProducts", productService.countAllAvailableProductsByName(dateFilter));
-                modelAndView.addObject("info", new Info("Produkty dostępne: <b>" + dateFilter + "</b>", true));
+                dateFilter = cart.getDate();
             }
+            modelAndView.addObject("countProducts", productService.countAllAvailableProductsByName(dateFilter));
+            modelAndView.addObject("info", new Info("Produkty dostępne: <b>" + dateFilter + "</b>", true));
+
         } else {
             if (dateFilter.isEmpty()) {
-                modelAndView.addObject("countProducts", productService.countAllAvailableProductsByNameFiltered(cart.getDate(), prodName));
-                modelAndView.addObject("info", new Info("Produkty zawierające frazę: <b>" + prodName + "</b> dostępne: <b>" + cart.getDate() + "</b>", true));
-            } else {
-                modelAndView.addObject("countProducts", productService.countAllAvailableProductsByNameFiltered(dateFilter, prodName));
-                modelAndView.addObject("info", new Info("Produkty zawierające frazę: <b>" + prodName + "</b> dostępne: <b>" + dateFilter + "</b>", true));
+                dateFilter = cart.getDate();
             }
-
+            modelAndView.addObject("countProducts", productService.countAllAvailableProductsByNameFiltered(dateFilter, prodName));
+            modelAndView.addObject("info", new Info("Produkty zawierające frazę: <b>" + prodName + "</b> dostępne: <b>" + dateFilter + "</b>", true));
         }
 
         cartService.addDateToCart(cart, dateFilter);
