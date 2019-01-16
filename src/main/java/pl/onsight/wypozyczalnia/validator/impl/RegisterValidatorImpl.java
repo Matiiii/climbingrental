fad09b3a96373683ce0d0474a6176936fff42beb
Jetoxixoxorix <technoamfa13@gmail.com>
2019-01-16 12:35:58 +0100

    caluclating number of days for cart

---
+++ src/main/java/pl/onsight/wypozyczalnia/validator/impl/RegisterValidatorImpl.java
@@ -0,0 +1,29 @@
+package pl.onsight.wypozyczalnia.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import pl.onsight.wypozyczalnia.model.entity.UserEntity;
+import pl.onsight.wypozyczalnia.service.UserService;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class RegisterValidatorImpl {
+
+
+    private UserService userService;
+
+    @Autowired
+    public RegisterValidatorImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    public boolean isEmailTaken(UserEntity userEntity) {
+        String email = userEntity.getEmail();
+        List<UserEntity> allUsers = userService.findAllUsers();
+        List<String> allEmails = allUsers.stream().map(UserEntity::getEmail).collect(Collectors.toList());
+        return allEmails.contains(email);
+    }
+}
