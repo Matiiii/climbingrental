f4f9e7fe1f0e428e15ca8993d499dabbd3f971db
Jetoxixoxorix <technoamfa13@gmail.com>
2019-01-18 09:29:00 +0100

    removed not used methods from ProductOrderService

--- src/main/java/pl/onsight/wypozyczalnia/service/ProductOrderService.java
+++ src/main/java/pl/onsight/wypozyczalnia/service/ProductOrderService.java
@@ -9,16 +9,6 @@ import java.util.List;
 public interface ProductOrderService {
     void saveOrder(ProductOrderEntity order);
 
-    List<ProductOrderEntity> findProductsByUserId(Long id);
-
-    ProductOrderEntity getPriceOfOrderedProduct(ProductOrderEntity productOrder);
-
-    Double getPriceOfOrderedProducts(List<ProductOrderEntity> productOrders);
-
-    boolean isProductAvailableToOrder(Long id, Date productOrderStart, Date productOrderEnd);
-
-    boolean isProductAvailableToOrder(Long id, String dateFilter) throws ParseException;
-
     Integer countOrdersProductInPeriod(Long productId, Date productOrderStart, Date productOrderEnd);
 
     List<String> getListOfDatesWhenProductIsReserved(Long id);
--- src/main/java/pl/onsight/wypozyczalnia/service/impl/ProductOrderServiceImpl.java
+++ src/main/java/pl/onsight/wypozyczalnia/service/impl/ProductOrderServiceImpl.java
@@ -3,16 +3,17 @@ package pl.onsight.wypozyczalnia.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pl.onsight.wypozyczalnia.DateFilter;
 import pl.onsight.wypozyczalnia.model.entity.ProductEntity;
 import pl.onsight.wypozyczalnia.model.entity.ProductOrderEntity;
 import pl.onsight.wypozyczalnia.repository.ProductOrderRepository;
 import pl.onsight.wypozyczalnia.repository.ProductRepository;
 import pl.onsight.wypozyczalnia.service.ProductOrderService;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,30 +34,6 @@ public class ProductOrderServiceImpl implements ProductOrderService {
     }
 
     @Override
-    public List<ProductOrderEntity> findProductsByUserId(Long id) {
-        return productOrderRepository.findByUserId(id);
-    }
-
-    @Override
-    public ProductOrderEntity getPriceOfOrderedProduct(ProductOrderEntity productOrder) {
-        //Double price = productOrder.getProductId().getPrice();
-        Long productOrderStartTime = productOrder.getOrderStart().getTime();
-        Long productOrderEndTime = productOrder.getOrderEnd().getTime();
-        Long numberOfMillisecondsInDay = (long) (1000 * 60 * 60 * 24);
-        Double lengthOfOrder = (double) (productOrderEndTime - productOrderStartTime + numberOfMillisecondsInDay) / numberOfMillisecondsInDay;
-        //productOrder.setCombinedPrice(price * lengthOfOrder);
-        return productOrder;
-    }
-
-    @Override
-    public Double getPriceOfOrderedProducts(List<ProductOrderEntity> productOrders) {
-        Double[] sum = {0.0};
-        // productOrders.forEach(product -> sum[0] += product.getCombinedPrice());
-
-        return sum[0];
-    }
-
-    @Override
     public Integer countOrdersProductInPeriod(Long productId, Date productOrderStart, Date productOrderEnd) {
         List<ProductOrderEntity> orders = findProductOrdersByProductId(productId);
         int count = 0;
@@ -73,24 +50,7 @@ public class ProductOrderServiceImpl implements ProductOrderService {
 
         return count;
     }
-
-    @Override
-    public boolean isProductAvailableToOrder(Long productId, String dateFilter) throws ParseException {
-        Date[] dates = DateFilter.changeStringToDate(dateFilter);
-        Date productOrderStart = dates[0];
-        Date productOrderEnd = dates[1];
-
-        return isProductAvailableToOrder(productId, productOrderStart, productOrderEnd);
-    }
-
-    @Override
-    public boolean isProductAvailableToOrder(Long productId, Date productOrderStart, Date productOrderEnd) {
-        ProductEntity product = productRepository.findOne(productId);
-        Integer countOrders = countOrdersProductInPeriod(productId, productOrderStart, productOrderEnd);
-
-        return countOrders < product.getQuantity();
-    }
-
+    
     @Override
     public List<String> getListOfDatesWhenProductIsReserved(Long id) {
         List<String> dates = new ArrayList<>();
