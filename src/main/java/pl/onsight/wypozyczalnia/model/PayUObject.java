294d042f9b6665191b0938a1c83b012115117f61
Jetoxixoxorix <technoamfa13@gmail.com>
2019-02-17 20:50:12 +0100

    getters and setters

--- src/main/java/pl/onsight/wypozyczalnia/model/PayUObject.java
+++ src/main/java/pl/onsight/wypozyczalnia/model/PayUObject.java
@@ -7,19 +7,123 @@ import java.util.List;
 public class PayUObject {
 
     //required
-    String customerIp;
-    String merchantPosId;
-    String description;
-    String currencyCode;
-    String totalAmount;
-    String buyer;
-    List<ProductEntity> products;
+    private String customerIp;
+    private String merchantPosId;
+    private String description;
+    private String currencyCode;
+    private String totalAmount;
+    private String buyer;
+    private List<ProductEntity> products;
 
     //not required
-    String extOrderId;
-    String notifyUrl;
-    String validityTime;
-    String additionalDescription;
-    String continueUrl;
-    String payMethods;
+    private String extOrderId;
+    private String notifyUrl;
+    private String validityTime;
+    private String additionalDescription;
+    private String continueUrl;
+    private String payMethods;
+
+    public String getCustomerIp() {
+        return customerIp;
+    }
+
+    public void setCustomerIp(String customerIp) {
+        this.customerIp = customerIp;
+    }
+
+    public String getMerchantPosId() {
+        return merchantPosId;
+    }
+
+    public void setMerchantPosId(String merchantPosId) {
+        this.merchantPosId = merchantPosId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public String getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(String totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(String buyer) {
+        this.buyer = buyer;
+    }
+
+    public List<ProductEntity> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductEntity> products) {
+        this.products = products;
+    }
+
+    public String getExtOrderId() {
+        return extOrderId;
+    }
+
+    public void setExtOrderId(String extOrderId) {
+        this.extOrderId = extOrderId;
+    }
+
+    public String getNotifyUrl() {
+        return notifyUrl;
+    }
+
+    public void setNotifyUrl(String notifyUrl) {
+        this.notifyUrl = notifyUrl;
+    }
+
+    public String getValidityTime() {
+        return validityTime;
+    }
+
+    public void setValidityTime(String validityTime) {
+        this.validityTime = validityTime;
+    }
+
+    public String getAdditionalDescription() {
+        return additionalDescription;
+    }
+
+    public void setAdditionalDescription(String additionalDescription) {
+        this.additionalDescription = additionalDescription;
+    }
+
+    public String getContinueUrl() {
+        return continueUrl;
+    }
+
+    public void setContinueUrl(String continueUrl) {
+        this.continueUrl = continueUrl;
+    }
+
+    public String getPayMethods() {
+        return payMethods;
+    }
+
+    public void setPayMethods(String payMethods) {
+        this.payMethods = payMethods;
+    }
 }
