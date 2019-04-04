7517c8902c33208e98ea742479c84a83f9694bac
Jetoxixoxorix <technoamfa13@gmail.com>
2019-04-04 20:37:42 +0200

    status 302 working redirecting to payu

--- src/main/java/pl/onsight/wypozyczalnia/service/impl/PayUServiceImpl.java
+++ src/main/java/pl/onsight/wypozyczalnia/service/impl/PayUServiceImpl.java
@@ -1,5 +1,6 @@
 package pl.onsight.wypozyczalnia.service.impl;
 
+import org.glassfish.jersey.client.ClientProperties;
 import org.springframework.stereotype.Service;
 import pl.onsight.wypozyczalnia.service.PayUService;
 
@@ -16,11 +17,7 @@ public class PayUServiceImpl implements PayUService {
 
     @Override
     public Response getToken() {
-        Client client = ClientBuilder.newClient();
-        //Entity<String> payload = Entity.text("grant_type=trusted_merchant&#38;client_id=145227&#38;client_secret=12f071174cb7eb79d4aac5bc2f07563f&#38;email=testaccount1%40devil.allegro.pl&#38;ext_customer_id=extCustId6667");
-        //Entity<String> payload = Entity.text("grant_type=client_credentials&#38;client_id=145227&#38;client_secret=12f071174cb7eb79d4aac5bc2f07563f&#38;email=testaccount1%40devil.allegro.pl&#38;ext_customer_id=extCustId6667");
-        //Entity<String> payload = Entity.text("grant_type=client_credentials&client_id=145227&client_secret=12f071174cb7eb79d4aac5bc2f07563f");
-        //Entity<String> payload = Entity.text("grant_type=client_credentials&client_id=300746&client_secret=2ee86a66e5d97e3fadc400c9f19b065d");
+        Client client = ClientBuilder.newClient().property(ClientProperties.FOLLOW_REDIRECTS, Boolean.FALSE);
 
         //testowy sklep na sandboxie
         Entity<String> payload = Entity.text("grant_type=client_credentials&client_id=352643&client_secret=f15e8ffa7a663cee2b910a44ba607f36");
@@ -28,15 +25,6 @@ public class PayUServiceImpl implements PayUService {
         Response response = client.target("https://secure.snd.payu.com/pl/standard/user/oauth/authorize?grant_type=client_credentials&client_id=352643&client_secret=f15e8ffa7a663cee2b910a44ba607f36")
                         .request(MediaType.APPLICATION_JSON_TYPE)
                         .post(payload);
-
- /*       Response response = client.target("https://secure.payu.com/pl/standard/user/oauth/authorize?grant_type=client_credentials&client_id=300746&client_secret=2ee86a66e5d97e3fadc400c9f19b065d")
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .post(payload);
-*/
-
-      /*  Response response = client.target("https://private-anon-c8d7694ad3-payu21.apiary-mock.com/pl/standard/user/oauth/authorize ")
-                .request(MediaType.TEXT_PLAIN_TYPE)
-                .post(payload);*/
 
         System.out.println("status: " + response.getStatus());
         System.out.println("headers: " + response.getHeaders());
@@ -47,13 +35,23 @@ public class PayUServiceImpl implements PayUService {
 
     @Override
     public Response createOrder() {
-        Client client = ClientBuilder.newClient();
-        Entity payload = Entity.json("{  \"notifyUrl\": \"https://asd.pl\"," +
+        Client client = ClientBuilder.newClient().property(ClientProperties.FOLLOW_REDIRECTS, Boolean.FALSE);
+        Entity payload = Entity.json("{  \"notifyUrl\": \"https://asd.pl/notify\"," +
                 "  \"customerIp\": \"127.0.0.1\"," +
                 "  \"merchantPosId\": \"352643\"," +
                 "  \"description\": \"RTV market\"," +
                 "  \"currencyCode\": \"PLN\"," +
                 "  \"totalAmount\": \"21000\"," +
+                "  \"buyer\": {" +
+                "  \"email\": \"john.doe@example.com\"," +
+                "  \"phone\": \"654111654\"," +
+                "  \"firstName\": \"John\"," +
+                "  \"lastName\": \"Doe\", " +
+                "  \"language\": \"pl\"" +
+                "  }," +
+                "  \"settings\":{" +
+                "  \"invoiceDisabled\": \"true\"" +
+                "   }," +
                 "  \"products\": [    {" +
                 "      \"name\": \"Wireless mouse\"," +
                 "      \"unitPrice\": \"15000\"," +
